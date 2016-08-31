0b7c2f7d452b5c4db2f5a78c8798ce822770208f
Adrien Aubry | drich <dridri85@gmail.com>
2016-09-01 01:35:00 +0200

    [camera_live sample]Use video port 71 instead of preview port 70 on camera

--- samples/camera_live.cpp
+++ samples/camera_live.cpp
@@ -11,8 +11,7 @@ int main( int ac, char** av )
 
 	Camera* camera = new Camera( 1920, 1080, 0, true, true );
 	VideoRender* render = new VideoRender( true );
-	camera->SetupTunnel( 70, render, 90 );
-// 	camera->SetupTunnel( 71, render, 90 );
+	camera->SetupTunnel( 71, render, 90 );
 
 	camera->SetState( Component::StateExecuting );
 	render->SetState( Component::StateExecuting );
